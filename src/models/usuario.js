0687e503b2470f6e60f63808f4f1fe6000a55c0a
Fernando <cmnando.silva@gmail.com>
2020-12-01 20:24:10 -0300

    Models usuario feitas

---
+++ src/models/usuario.js
@@ -0,0 +1,22 @@
+const { getByDisplayValue } = require("@testing-library/react");
+const connection = require("../database/connection");
+
+module.exports = {
+    async create(usuario) {
+        const result = await connection("usuario").insert(usuario);
+        return result;
+    },
+
+    async getById({ user_id }) {
+        const result = await connection("usuario").where({ user_id }).select("*");
+        return result;
+    },
+
+    async updateById(user_id, usuario) {
+        const result = await connection("usuario").where(user_id).update(usuario);
+        return result;
+    },
+    async deleteById(user_id) {
+        const result = await connection("usuario").where({ user_id }).delete();
+    }
+}
