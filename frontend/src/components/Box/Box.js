de2067e9d25fbe9b20dd15588c07f68aaa4505d5
Fernando Cesar <cmnando.silva@gmail.com>
2020-12-17 17:46:40 -0300

    Integração do cadastro com o firebase feita

--- frontend/src/components/Box/Box.js
+++ frontend/src/components/Box/Box.js
@@ -1,40 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import './Box.css';
-import { BiArrowBack } from 'react-icons/bi';
-import { Button } from 'react-bootstrap';
-import Input from '../input';
-import api from '../../services/api';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "./Box.css";
+import { BiArrowBack } from "react-icons/bi";
+import { Button } from "react-bootstrap";
+import Input from "../input";
+import api from "../../services/api";
 
 function Box({
-  title, subtitle, nextMessage, nextButton, input, nome, email, password,
-  confirmEmail, confirmPassword, setNome, setEmail, setSenha, setConfirmEmail, setConfirmPassword,
+  title,
+  subtitle,
+  nextMessage,
+  nextButton,
+  input,
+  nome,
+  email,
+  password,
+  senha,
+  confirmEmail,
+  confirmPassword,
+  setNome,
+  setEmail,
+  setSenha,
+  setConfirmEmail,
+  setConfirmPassword,
 }) {
   const history = useHistory();
   const [users, setUsers] = useState([]);
-  const url = nextButton === 'Cadastrar' ? '/img/Icones/novofundo_login.png' : '/img/Icones/novofundo-cadastro.png';
-  const marginNextButton = nextButton === 'Cadastrar' ? '50px' : '0px';
+  const url =
+    nextButton === "Cadastrar"
+      ? "/img/Icones/novofundo_login.png"
+      : "/img/Icones/novofundo-cadastro.png";
+  const marginNextButton = nextButton === "Cadastrar" ? "50px" : "0px";
+  
+  async function create(objetoCadastro) {
+    console.log(objetoCadastro);
+    const objetoBackend = {
+      nome,
+      email,
+      senha,
+    };
+    objetoBackend.nome = objetoCadastro.nome;
+    objetoBackend.email = objetoCadastro.email;
+    objetoBackend.senha = objetoCadastro.password;
+    
+    try{
+      await api.post("usuario", objetoBackend);      
+    }
+    catch{
+      alert('Erro!');
+    }
 
-  useEffect(() => {
-    const URL = 'http://localhost:8080/users';
-    fetch(URL)
-      .then(async (respostaDoServer) => {
-        if (respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setUsers([
-            ...resposta,
-          ]);
-          return;
-        }
-        throw new Error('Não foi possível pegar os dados');
-      });
-  }, []);
-
-  function create(objetoCadastro) {
-    alert(nome);
-    alert(email);
-    alert(password);
     /*axios.post('http://localhost:8080/users', objetoCadastro)
       .then(alert('Cadastrado com sucesso!'))
       .catch(() => {
@@ -47,46 +62,50 @@ function Box({
       nome,
       email,
       password,
-      admin: false,
     });
   };
 
   const handleClick = () => {
-    if (nextButton === 'Cadastrar') {
+    if (nextButton === "Cadastrar") {
       const userEmail = email;
       const userPassword = password;
       const existUser = users.filter((item) => item.email === userEmail);
       if (existUser.length !== 0) {
-        if (existUser[0].password === userPassword) history.push('/');
-        else alert('Informações incorretas!');
+        if (existUser[0].password === userPassword) history.push("/");
+        else alert("Informações incorretas!");
       } else {
-        alert('Usuário não cadastrado!');
+        alert("Usuário não cadastrado!");
       }
     } else {
       const isEqualsEmails = email === confirmEmail;
       const isEqualsPasswords = password === confirmPassword;
       const alreadyRegister = users.filter((item) => item.email === email);
       if (alreadyRegister.length !== 0) {
-        alert('Usuário ja cadastrado');
+        alert("Usuário ja cadastrado");
       } else if (isEqualsEmails && isEqualsPasswords) {
         registerUser();
-        history.push('/login');
-      } else alert('Email ou senha não coincidem com confirmação');
+        history.push("/login");
+      } else alert("Email ou senha não coincidem com confirmação");
     }
   };
 
   return (
-    <div className="containerRegister" style={{ backgroundImage: `url(${url})` }}>
+    <div
+      className="containerRegister"
+      style={{ backgroundImage: `url(${url})` }}
+    >
       <div className="baseRegister">
         <div className="boxContainer">
-          <BiArrowBack className="back_button" type="button" onClick={() => history.push('/home')} />
+          <BiArrowBack
+            className="back_button"
+            type="button"
+            onClick={() => history.push("/home")}
+          />
           <div className="logoAzul" />
         </div>
         <div>
           <h2 className="H2title">{title}</h2>
-          <p className="text">
-            {subtitle}
-          </p>
+          <p className="text">{subtitle}</p>
           <Input
             input={input}
             setNome={setNome}
@@ -96,22 +115,25 @@ function Box({
             setConfirmPassword={setConfirmPassword}
           />
 
-          {nextButton === 'Cadastrar'
-          && (
-          <div className="fundocontainer1" alt="Help">
-            <Button variant="link">Esqueceu a senha?</Button>
-          </div>
+          {nextButton === "Cadastrar" && (
+            <div className="fundocontainer1" alt="Help">
+              <Button variant="link">Esqueceu a senha?</Button>
+            </div>
           )}
 
-          {nextButton === 'Logar' && (
-          <p className="subtitle">
-            Ao se cadastrar, você aceita os Termos de Serviço e a Política de
-            Privacidade
-          </p>
+          {nextButton === "Logar" && (
+            <p className="subtitle">
+              Ao se cadastrar, você aceita os Termos de Serviço e a Política de
+              Privacidade
+            </p>
           )}
 
           <div className="next_button" style={{ marginTop: marginNextButton }}>
-            <Button variant="primary" type="Proximo" onClick={() => handleClick()}>
+            <Button
+              variant="primary"
+              type="Proximo"
+              onClick={() => handleClick()}
+            >
               Próximo
             </Button>
           </div>
@@ -120,8 +142,8 @@ function Box({
             <Button
               variant="link"
               onClick={() => {
-                if (nextButton === 'Cadastrar') history.push('/cadastro');
-                else history.push('/login');
+                if (nextButton === "Cadastrar") history.push("/cadastro");
+                else history.push("/login");
               }}
             >
               {nextButton}
